c2787b49cfc694fbb6b4758a1d5116bf94cdffd1
Jorge Galvis <jorlugaqui@gmail.com>
2023-11-25 12:11:10 -0500

    chore: Add web service (WIP)

--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
+from rag_weaviate import chain as rag_weaviate_chain
 
 app = FastAPI()
 
@@ -9,9 +10,8 @@ app = FastAPI()
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
-
 # Edit this to add the chain you want to add
-add_routes(app, NotImplemented)
+add_routes(app, rag_weaviate_chain, path="/rag-weaviate")
 
 if __name__ == "__main__":
     import uvicorn
